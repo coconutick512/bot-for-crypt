e8818695b640f1d6306ca402a5063cfd8a88ff67
vlrqx <ayecamoninja@mail.ru>
2025-09-04 20:19:01 +0300

    Прописали .env + обновили index.js + Обновили db.js

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const { Sequelize, DataTypes, Op } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(proccess.env.DATABASE_URL, {
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
   dialect: "postgres",
   logging: false,
 });
